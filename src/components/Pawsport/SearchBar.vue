44f7eaea9b56ea880c637bf9f818bdfbeb96fc88
Teddie <sky68452@gmail.com>
2026-05-08 16:23:06 +0800

    style: 調整搜尋欄的邊框顏色

--- src/components/Pawsport/SearchBar.vue
+++ src/components/Pawsport/SearchBar.vue
@@ -49,7 +49,7 @@ const handleEnter = () => {
         placeholder="搜尋玩家名稱或 ID..."
         v-model="keyword"
         @keyup.enter="handleEnter"
-        class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pr-14 pl-12 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[430px] dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30" />
+        class="dark:bg-dark-900 shadow-theme-xs focus:border-theme-info-300 focus:ring-brand-500/10 dark:focus:border-theme-info-800 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pr-14 pl-12 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[430px] dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30" />
 
       <button 
         v-if="keyword"
