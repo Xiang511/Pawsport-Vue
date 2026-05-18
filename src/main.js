8fbff80c12d41f6e5e8fbef8918c9c4a05edcf2e
Xiang511 <toby91511@gmail.com>
2026-05-18 13:34:52 +0800

    feat: main.js 引入 pinia

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import { createApp } from 'vue'
 
 import { createPinia } from 'pinia'
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import './assets/Tailadmin/main.css'
 import router from './router'
 
-// 現在從 ./router/index.js 導入
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
