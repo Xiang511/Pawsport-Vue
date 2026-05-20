c188898fc110830bf77135005edb8a5c482e72d5
Xiang511 <toby91511@gmail.com>
2026-05-21 00:12:24 +0800

    fix: 登出時路由跳轉錯誤

--- src/components/Tailadmin/layout/header/UserMenu.vue
+++ src/components/Tailadmin/layout/header/UserMenu.vue
@@ -91,7 +91,7 @@ onUnmounted(() => {
         </li>
       </ul>
       <router-link
-        to="/signin"
+        to="/dashboard/login"
         @click="signOut"
         class="group text-theme-sm mt-3 flex items-center gap-3 rounded-lg px-3 py-2 font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300">
         <LogoutIcon
