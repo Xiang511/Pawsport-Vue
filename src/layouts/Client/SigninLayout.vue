6ac58669e0bb1edd3b93ebcb513ca26a7e0aba80
Xiang511 <toby91511@gmail.com>
2026-05-23 23:03:18 +0800

    fix: 修正 replace 後頁面不會刷新的問題
    - Router 加上 :Key

--- src/layouts/Client/SigninLayout.vue
+++ src/layouts/Client/SigninLayout.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 
 <template>
-  <RouterView />
+  <RouterView :key="route.fullPath" />
 </template>
 
 <style scoped></style>
